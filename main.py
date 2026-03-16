0f10a92007f87292ad36d132efc98fb9b54c212a
neeti2k-CSAI75 <neeti2k@gmail.com>
2026-03-16 23:42:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
-import pandas as pd
 
 st.title("Climate Change Comparative Analysis Dashboard")
 
 st.write("This dashboard shows climate change data using pie charts and bar graphs.")
+user=st.text_input('ENTER USER NAME')
+if st.button('CLICK'):
+  st.write('WELCOME TO CLIMATE CHANGE APP')
