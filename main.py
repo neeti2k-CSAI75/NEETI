ffb07721fa59b24dcb5c36f00cc41a27588f219e
neeti2k-CSAI75 <neeti2k@gmail.com>
2026-03-16 23:27:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+
 st.title("Climate Change Comparative Analysis Dashboard")
 
 st.write("This dashboard shows climate change data using pie charts and bar graphs.")
