beb7a1732477b60e3d26bbc7bdf9cb776cd8e2c0
neeti2k-CSAI75 <neeti2k@gmail.com>
2026-03-16 23:52:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Climate Change Comparative Analysis Dashboard")
-st.markdown("<h1 style='font-size:20px;'>Welcome to My App</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='color:purple;font-size:20px;'>Welcome to My App</h1>", unsafe_allow_html=True)
 
 st.write("This dashboard shows climate change data using pie charts and bar graphs.")
 user=st.text_input('ENTER USER NAME')
