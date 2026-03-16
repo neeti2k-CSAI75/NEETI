96f31ccff20fb628baad14ef821cf058a359cfcc
neeti2k-CSAI75 <neeti2k@gmail.com>
2026-03-16 23:44:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ st.title("Climate Change Comparative Analysis Dashboard")
 st.write("This dashboard shows climate change data using pie charts and bar graphs.")
 user=st.text_input('ENTER USER NAME')
 if st.button('CLICK'):
-  st.write('WELCOME TO CLIMATE CHANGE APP')
+  st.write(user,'WELCOME TO CLIMATE CHANGE APP')
